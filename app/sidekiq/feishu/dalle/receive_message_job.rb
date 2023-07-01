2b500dd81e7270c7d09b81073222f0d1f6d0a4d2
Pan Jie <fenqi@startdt.com>
2023-07-01 23:27:10 +0800

    style: format perform function for dalle

--- app/sidekiq/feishu/dalle/receive_message_job.rb
+++ app/sidekiq/feishu/dalle/receive_message_job.rb
@@ -3,18 +3,12 @@ class Feishu::DALLE::ReceiveMessageJob
 
   def perform(message_id)
     message = JSON.parse(Rails.cache.read(message_id))
-
     openai_client = OpenAI::Client.new
     dalle_client = Feishu::DALLE.new
-
-    prompt = JSON.parse(message["content"])["text"]
-    response = openai_client.images.generate(parameters: { prompt: prompt })
-    image_url = response.dig("data", 0, "url")
+    prompt = JSON.parse(message['content'])['text']
+    response = openai_client.images.generate(parameters: { prompt: })
+    image_url = response.dig('data', 0, 'url')
     image_key = dalle_client.upload_image_by_url(image_url)
-    dalle_client.reply_message(
-      message_id,
-      { "image_key" => image_key },
-      "image"
-    )
+    dalle_client.reply_message(message_id, { image_key: }, 'image')
   end
 end
