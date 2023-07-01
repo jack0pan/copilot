cc2ad0baaaed1b6608ea47e93d04642e18dd5f4a
Pan Jie <fenqi@startdt.com>
2023-07-01 17:28:35 +0800

    style: prefer single-quoted strings

--- app/sidekiq/feishu/chatgpt/receive_message_job.rb
+++ app/sidekiq/feishu/chatgpt/receive_message_job.rb
@@ -18,9 +18,9 @@ class Feishu::ChatGPT::ReceiveMessageJob
     response = openai_client.create_chat_completion(chat_messages(message, feishu_client))
 
     feishu_client.reply_message(
-      message["message_id"],
+      message['message_id'],
       {
-        text: response.body.dig("choices", 0, "message", "content")
+        text: response.body.dig('choices', 0, 'message', 'content')
       }
     )
   end
@@ -28,34 +28,34 @@ class Feishu::ChatGPT::ReceiveMessageJob
   private
 
   def chat_messages(message, feishu_client)
-    content = JSON.parse(message["content"])["text"]
-    strip_mention!(content, message["mentions"]) if message["mentions"]
+    content = JSON.parse(message['content'])['text']
+    strip_mention!(content, message['mentions']) if message['mentions']
 
-    gpt_messages = [{ role: "user", content: content }]
+    gpt_messages = [{ role: 'user', content: }]
 
     p_message = message
-    while p_message["parent_id"]
-      p_message = feishu_client.get_message(p_message["parent_id"])
-      type = p_message["message_type"] || p_message["msg_type"]
-      if type == "text"
-        role =
-          p_message["sender"]["sender_type"] == "user" ? "user" : "assistant"
-        text = JSON.parse(p_message["body"]["content"])["text"]
-        strip_mention!(text, p_message["mentions"]) if p_message["mentions"]
-        gpt_messages.unshift({ role: role, content: text })
-      end
+    while p_message['parent_id']
+      p_message = feishu_client.get_message(p_message['parent_id'])
+      type = p_message['message_type'] || p_message['msg_type']
+      next unless type == 'text'
+
+      role =
+        p_message['sender']['sender_type'] == 'user' ? 'user' : 'assistant'
+      text = JSON.parse(p_message['body']['content'])['text']
+      strip_mention!(text, p_message['mentions']) if p_message['mentions']
+      gpt_messages.unshift({ role:, content: text })
     end
 
-    if message["chat_type"] == "group"
-      chat = feishu_client.get_chat(message["chat_id"])
-      gpt_messages.unshift({ role: "system", content: chat["description"] })
+    if message['chat_type'] == 'group'
+      chat = feishu_client.get_chat(message['chat_id'])
+      gpt_messages.unshift({ role: 'system', content: chat['description'] })
     end
 
     gpt_messages
   end
 
   def strip_mention!(text, mentions)
-    mentions.each { |mention| text.gsub!(mention["key"], "") }
+    mentions.each { |mention| text.gsub!(mention['key'], '') }
     text.strip!
   end
 end
